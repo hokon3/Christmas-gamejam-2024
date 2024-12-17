a1f61457bd41aad60d29a6beb1b732e91398e707
håkon <hakomols@yahoo.no>
2024-12-17 20:09:45 +0100

    version 1

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,30 +3,52 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float m_Thrust = 20f;
+    public float m_Thrust;
     public Rigidbody Rigidbody;
     private InputAction moveAction;
+    public GameObject victoryText;
+    private bool gotNose = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        Rigidbody.maxLinearVelocity = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        if (moveValue.y != 0)
+        if (!gotNose)
         {
-            var vector = Vector3.ClampMagnitude(transform.forward * m_Thrust, 1);
-            //vector.y *= moveValue.y;
-            Rigidbody.AddForce(vector);
-        }
-        if (moveValue.x != 0)
-        {
-            transform.Rotate(new Vector3(0, moveValue.x, 0));
+            Vector2 moveValue = moveAction.ReadValue<Vector2>();
+            if (moveValue.y != 0)
+            {
+                var vector = transform.forward * m_Thrust;
+                vector *= moveValue.y;
+                Rigidbody.AddForce(vector);
+            }
+            if (moveValue.x != 0)
+            {
+                transform.Rotate(new Vector3(0, moveValue.x, 0));
+            }
         }
 
-        ;
+        if (Input.GetKeyDown(KeyCode.Escape)) { 
+            Application.Quit();
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Nose")
+        {
+            victoryText.SetActive(true);
+            gotNose = true;
+        }
+    }
+
+    public void startGame()
+    {
+        gotNose = false;
     }
 }
